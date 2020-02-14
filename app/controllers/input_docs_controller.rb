62e62ffb0e233a32815b3f806b93ae3e29b9f2be
Niv Dadush <niv@securitize.io>
2020-02-14 23:34:15 +0200

    docx upload only

--- app/controllers/input_docs_controller.rb
+++ app/controllers/input_docs_controller.rb
@@ -1,13 +1,30 @@
 class InputDocsController < ApplicationController
   def index
+    @input_docs = InputDoc.all
   end
 
   def new
+    @input_doc = InputDoc.new
   end
 
   def create
+    @input_doc = InputDoc.new(input_doc_params)
+    if @input_doc.save
+      redirect_to input_docs_path, notice: "Successfully uploaded."
+    else
+      render "new"
+    end
   end
 
   def destroy
+    @input_doc = InputDoc.find(params[:id])
+    @input_doc.destroy
+    redirect_to input_docs_path, notice:  "Successfully deleted."
   end
+
+  private
+  def input_doc_params
+    params.require(:input_doc).permit(:name, :attachment)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'input_docs/index'
-  get 'input_docs/new'
-  get 'input_docs/create'
-  get 'input_docs/destroy'
+  # get 'input_docs/index'
+  # get 'input_docs/new'
+  # get 'input_docs/create'
+  # get 'input_docs/destroy'
+  resources :input_docs, only: [:index, :new, :create, :destroy]
   root "input_docs#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
